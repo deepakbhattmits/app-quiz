0c2b694ca25fe7a31cc49c9557896e9f4db6e28b
deepak bhatt <deepakbhattmits@gmail.com>
2020-09-19 00:16:24 +0530

    Update App.js
    refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,25 @@
 /** @format */
 
-import React, { Component } from 'react';
-//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../assets/styles/index.scss';
+import React, { useState, useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { onQuizLoad, onPagerUpdate } from '../actions';
 import Quiz from './Quiz';
-import { connect } from 'react-redux';
-import { quizConstants } from '../constants';
-
-const mapStateToProps = (state) => {
-	return { ...state.quiz };
-};
-
-const mapDispatchToProps = (dispatch) => ({
-	onQuizLoad: (payload) => dispatch({ type: quizConstants.QuizLoad, payload }),
-	onPagerUpdate: (payload) =>
-		dispatch({ type: quizConstants.PagerUpdate, payload }),
-});
-
-class App extends Component {
-	state = {
+import '../assets/styles/index.scss';
+const App = () => {
+	const dispatch = useDispatch();
+	const quiz = useSelector((state) => state.quiz);
+	const [state, setState] = useState({
 		quizes: [{ id: '/data/javascript.json', name: 'Javascript' }],
 		quizId: '/data/javascript.json',
-	};
+	});
 
-	pager = {
-		index: 0,
-		size: 1,
-		count: 1,
-	};
-
-	componentDidMount() {
-		this.load(this.state.quizId);
-	}
-
-	load(quizId) {
-		let url = quizId || this.props.quizId;
+	const load = useCallback((quizId) => {
+		const pager = {
+			index: 0,
+			size: 1,
+			count: 1,
+		};
+		let url = quizId;
 		fetch(`../${url}`)
 			.then((res) => res.json())
 			.then((res) => {
@@ -42,42 +27,28 @@ class App extends Component {
 				quiz.questions.forEach((q) => {
 					q.options.forEach((o) => (o.selected = false));
 				});
-				quiz.config = Object.assign(this.props.quiz.config || {}, quiz.config);
-				this.pager.count = quiz.questions.length / this.pager.size;
-				this.props.onQuizLoad(quiz);
-				this.props.onPagerUpdate(this.pager);
+				quiz.config = Object.assign(quiz.config || {}, quiz.config);
+				pager.count = quiz.questions.length / pager.size;
+				dispatch(onQuizLoad(quiz));
+				dispatch(onPagerUpdate(pager));
 			});
-	}
+	}, []);
+	useEffect(() => {
+		load(state.quizId);
+	}, [load, state]);
 
-	//   onChange = (e) => {
-	//     this.setState({ quizId: e.target.value });
-	//     this.load(e.target.value);
-	//   }
-
-	render() {
-		return (
-			<div className='container'>
-				<header className='p-2'>
-					<div className='row'>
-						<div className='col-6'>
-							<h3>Quiz Application</h3>
-						</div>
-						{/* <div className="col-6 text-right">
-              <label className="mr-1">Select Quiz:</label>
-              <select onChange={this.onChange}>
-                {this.state.quizes.map(q => <option key={q.id} value={q.id}>{q.name}</option>)}
-              </select>
-            </div> */}
+	return (
+		<div className='container'>
+			<header className='p-2'>
+				<div className='row'>
+					<div className='col-6'>
+						<h3>Quiz Application</h3>
 					</div>
-				</header>
-				<Quiz
-					quiz={this.state.quiz}
-					quizId={this.state.quizId}
-					mode={this.state.mode}
-				/>
-			</div>
-		);
-	}
-}
+				</div>
+			</header>
+			<Quiz quiz={state.quiz} quizId={state.quizId} mode={state.mode} />
+		</div>
+	);
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
