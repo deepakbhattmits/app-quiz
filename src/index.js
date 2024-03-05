9349f840595f39d873f4ab27434d7f9ff6088272
deepakbhattmits <deepakbhattmits@gmail.com>
2024-03-05 20:27:21 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,41 +1,38 @@
 /** @format */
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import reduxThunk from 'redux-thunk';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import reducer from './reducer';
-import App from './components/App';
-import Result from './components/Result';
-import Review from './components/Review';
-import Home from './components/Home';
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { thunk } from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import reducer from './reducer'
+import App from './components/App'
+import Result from './components/Result'
+import Review from './components/Review'
+import Home from './components/Home'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(
-	reducer,
-	composeEnhancers(applyMiddleware(reduxThunk))
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
 function RoutingComponent() {
-	return (
-		<div>
-			<Switch>
-				<Route exact path='/' component={Home} />
-				<Route exact path='/quiz' component={App} />
-				<Route exact path='/quiz/review' component={Review} />
-				<Route exact path='/quiz/result' component={Result} />
-			</Switch>
-		</div>
-	);
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/quiz" element={<App />}></Route>
+        <Route path="/quiz/review" element={<Review />}></Route>
+        <Route path="/quiz/result" element={<Result />}></Route>
+      </Routes>
+    </div>
+  )
 }
-const rootElement = document.querySelector('#root');
-ReactDOM.render(
-	<Provider store={store}>
-		<Router>
-			<RoutingComponent />
-		</Router>
-	</Provider>,
-	rootElement
-);
+const rootElement = document.querySelector('#root')
+const root = createRoot(rootElement)
+root.render(
+  <Provider store={store}>
+    <Router>
+      <RoutingComponent />
+    </Router>
+  </Provider>,
+)
